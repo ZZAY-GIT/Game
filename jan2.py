5e1fe02e2467259b5cf8b00c0a6eb0a33a3cf73c
danya <Ampleenkov.do@gmail.com>
2023-01-15 14:44:35 +0300

    Переписал снова под спрайты

--- jan2.py
+++ jan2.py
@@ -1,115 +1,104 @@
 import pygame
+import pygame.mixer
+import random
 
-# Initialize pygame and set up the game window
+pygame.mixer.init()
 pygame.init()
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Arkanoid")
+pygame.display.set_caption("Арканоид")
+#СКАЧАЙ ЗВУК
+№collision_sound = pygame.mixer.Sound("123.mp3")
 
 
 # класс мячика
-class Ball:
-    def __init__(self, x, y, radius, speed):
-        self.x = x
-        self.y = y
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, radius, x, y):
+        super().__init__(all_sprites)
         self.radius = radius
-        self.speed = speed
-        self.dx = speed
-        self.dy = -speed
+        self.image = pygame.Surface((2 * radius, 2 * radius),
+                                    pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color("white"),
+                           (radius, radius), radius)
+        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
+        self.vx = 2
+        self.vy = -2
 
-    def draw(self, screen):
-        pygame.draw.circle(screen, (255, 255, 255), (self.x, self.y), self.radius)
-
-    def move(self):
-        self.x += self.dx
-        self.y += self.dy
-        if self.x + self.radius > width or self.x - self.radius < 0:
-            self.dx = -self.dx
-        if self.y - self.radius < 0:
-            self.dy = -self.dy
-        if self.y + self.radius >= height:
-            print('Проиграли!')
-            pygame.quit()
-
-    # проверка столкновения
-    def collides_with(self, obj):
-        if isinstance(obj, Paddle):
-            if (self.x > obj.x and self.x < obj.x + obj.width) and (self.y + self.radius > obj.y):
-                return True
-        elif isinstance(obj, Brick):
-            if (self.x + self.radius > obj.x) and (self.x - self.radius < obj.x + obj.width) and (
-                    self.y + self.radius > obj.y) and (self.y - self.radius < obj.y + obj.height):
-                return True
+    def update(self):
+        if 0 <= self.rect.x + self.vx <= width - self.radius:
+            self.rect = self.rect.move(self.vx, self.vy)
         else:
-            return False
-
-    def bounce(self):
-        self.dy = -self.dy
+            self.vx = -self.vx
+        if 0 >= self.rect.y - self.radius:
+            self.vy = -self.vy
+        if pygame.sprite.spritecollideany(self, paddle):
+            self.vy = -self.vy
+        if pygame.sprite.spritecollide(self, bricks, True):
+            collision_sound.play()
+            self.vy = -self.vy
 
 
 # класс платформы
-class Paddle:
-    def __init__(self, x, y, width, height, speed):
-        self.x = x
-        self.y = y
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, color):
+        super().__init__(all_sprites)
+        self.image = pygame.Surface((width, height))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
         self.width = width
         self.height = height
-        self.speed = speed
 
-    def draw(self, screen):
-        pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y, self.width, self.height))
-
-    def move_left(self):
-        if self.x >= 0:
-            self.x -= self.speed
-
-    def move_right(self):
-        if self.x <= width - self.width:
-            self.x += self.speed
+    def update(self, direction):
+        if direction == 'right':
+            if self.rect.x + 5 <= width - self.width:
+                self.rect = self.rect.move(5, 0)
+        else:
+            if self.rect.x - 5 >= 0:
+                self.rect = self.rect.move(-5, 0)
 
 
 # класс кирпича
-class Brick:
+class Brick(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
+        super().__init__(all_sprites)
+        self.image = pygame.Surface((width, height))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
         self.width = width
         self.height = height
         self.color = color
-        self.destroyed = False
 
-    def draw(self, screen):
-        if not self.destroyed:
-            pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
-            pygame.draw.rect(screen, (0, 0, 0), (self.x, self.y, self.width, self.height), 1)
-
-    def hit(self):
-        self.destroyed = True
-
-    def is_destroyed(self):
-        return self.destroyed
+    def update(self):
+        pass
 
 
 # создание кирпичей
 def create_bricks():
-    bricks = []
     colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
     x_pos = 50
     y_pos = 50
     for i in range(3):
-        for j in range((width-100)//50):
-            bricks.append(Brick(x_pos, y_pos, 50, 25, colors[i]))
+        for j in range((width - 100) // 50):
+            bricks.add(Brick(x_pos, y_pos, 50, 25, colors[i]))
             x_pos += 50
         x_pos = 50
         y_pos += 25
-    return bricks
 
 
 # создаём шарик, платформу и кирпичи
-ball = Ball(400, 280, 5, 2)
-paddle = Paddle(350, 500, 100, 10, 5)
-bricks = create_bricks()
+all_sprites = pygame.sprite.Group()
+paddle = pygame.sprite.Group()
+ball = Ball(7, 400, 280)
+paddle.add(Paddle(350, 510, 100, 10, (255, 255, 255)))
+bricks = pygame.sprite.Group()
+create_bricks()
 
+# Add the sprites to the group
+all_sprites.add(ball, bricks, paddle)
 clock = pygame.time.Clock()
 moving = False
 running = True
@@ -117,40 +106,32 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             moving = True
         if event.type == pygame.MOUSEBUTTONUP:
             moving = False
-    # передвижение с помощью клавиш
+
+    all_sprites.draw(screen)
+    ball.update()
+    # передвижение платформы клавишами
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        paddle.move_left()
+        paddle.update('left')
     if keys[pygame.K_RIGHT]:
-        paddle.move_right()
-    # передвижение с помощью мышки
+        paddle.update('right')
+
+    # передвижение платформы мышью
     if moving:
-        cursor_x, cursor_y = pygame.mouse.get_pos()
-        paddle.x = cursor_x - paddle.width // 2
-        if paddle.x < 0:
-            paddle.x = 0
-        elif paddle.x + paddle.width > width:
-            paddle.x = width - paddle.width
-    # шарик двигается и проверяет столкновение
-    ball.move()
-    if ball.collides_with(paddle):
-        ball.bounce()
-    for brick in bricks:
-        if ball.collides_with(brick):
-            ball.bounce()
-            brick.hit()
-            if brick.is_destroyed():
-                bricks.remove(brick)
-    screen.fill((0, 0, 0))
-    ball.draw(screen)
-    paddle.draw(screen)
-    for brick in bricks:
-        brick.draw(screen)
+        for i in paddle:
+            mouse_x, _ = pygame.mouse.get_pos()
+            i.rect.x = mouse_x - i.width // 2
+    # проверка кубиков
+
+
+
     pygame.display.update()
+    screen.fill('black')
     clock.tick(60)
 
 pygame.quit()
