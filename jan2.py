aa704286a83722e7d135245c99a74694959af78b
danya <Ampleenkov.do@gmail.com>
2023-01-15 13:10:35 +0300

    закоментил

--- jan2.py
+++ jan2.py
@@ -7,6 +7,7 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Arkanoid")
 
 
+# класс мячика
 class Ball:
     def __init__(self, x, y, radius, speed):
         self.x = x
@@ -22,12 +23,15 @@ class Ball:
     def move(self):
         self.x += self.dx
         self.y += self.dy
-
         if self.x + self.radius > width or self.x - self.radius < 0:
             self.dx = -self.dx
         if self.y - self.radius < 0:
             self.dy = -self.dy
+        if self.y + self.radius >= height:
+            print('Проиграли!')
+            pygame.quit()
 
+    # проверка столкновения
     def collides_with(self, obj):
         if isinstance(obj, Paddle):
             if (self.x > obj.x and self.x < obj.x + obj.width) and (self.y + self.radius > obj.y):
@@ -43,6 +47,7 @@ class Ball:
         self.dy = -self.dy
 
 
+# класс платформы
 class Paddle:
     def __init__(self, x, y, width, height, speed):
         self.x = x
@@ -63,6 +68,7 @@ class Paddle:
             self.x += self.speed
 
 
+# класс кирпича
 class Brick:
     def __init__(self, x, y, width, height, color):
         self.x = x
@@ -84,41 +90,52 @@ class Brick:
         return self.destroyed
 
 
+# создание кирпичей
 def create_bricks():
     bricks = []
     colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
-    x_pos = 0
+    x_pos = 50
     y_pos = 50
     for i in range(3):
-        for j in range(width//50):
+        for j in range((width-100)//50):
             bricks.append(Brick(x_pos, y_pos, 50, 25, colors[i]))
             x_pos += 50
-        x_pos = 0
+        x_pos = 50
         y_pos += 25
     return bricks
 
 
-# Create the ball, paddle, and bricks
+# создаём шарик, платформу и кирпичи
 ball = Ball(400, 280, 5, 2)
 paddle = Paddle(350, 500, 100, 10, 5)
 bricks = create_bricks()
 
 clock = pygame.time.Clock()
-# Main game loop
+moving = False
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # Move the paddle based on user input
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            moving = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            moving = False
+    # передвижение с помощью клавиш
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         paddle.move_left()
     if keys[pygame.K_RIGHT]:
         paddle.move_right()
-
-    # Move the ball and check for collisions
+    # передвижение с помощью мышки
+    if moving:
+        cursor_x, cursor_y = pygame.mouse.get_pos()
+        paddle.x = cursor_x - paddle.width // 2
+        if paddle.x < 0:
+            paddle.x = 0
+        elif paddle.x + paddle.width > width:
+            paddle.x = width - paddle.width
+    # шарик двигается и проверяет столкновение
     ball.move()
     if ball.collides_with(paddle):
         ball.bounce()
@@ -128,15 +145,12 @@ while running:
             brick.hit()
             if brick.is_destroyed():
                 bricks.remove(brick)
-
-    # Draw the game elements to the screen
     screen.fill((0, 0, 0))
     ball.draw(screen)
     paddle.draw(screen)
     for brick in bricks:
         brick.draw(screen)
-
     pygame.display.update()
     clock.tick(60)
-# Clean up and exit
+
 pygame.quit()
